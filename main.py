57c247a526ede2dfda5c898579271c019dd4e5c6
davauntaraa <davauntaraa@gmail.com>
2026-06-19 20:43:28 +0700

    update

--- main.py
+++ main.py
@@ -1,45 +1,51 @@
-from module import SistemAntreanGigi
+from module import SistemAntreanGigi, clear, header
 
 def main():
     sistem = SistemAntreanGigi()
 
     while True:
-        print("\n" + "=" * 40)
-        print(" SISTEM ANTREAN KLINIK GIGI ")
-        print("=" * 40)
+        clear()
+        header("SISTEM ANTREAN KLINIK GIGI")
         print("1. Tambah Pasien")
         print("2. Lihat Antrean")
         print("3. Layani Pasien")
         print("4. Cari Pasien")
         print("5. Hitung Pasien")
         print("6. Keluar")
-        print("=" * 40)
+        print("=" * 90)
 
         pilihan = input("Pilih menu: ")
 
         if pilihan == "1":
-            print("\n" + "=" * 40)
-            print(" TAMBAH PASIEN BARU ")
-            print("=" * 40)
+            header("TAMBAH PASIEN BARU")
 
             nama = input("Nama Pasien: ")
 
-            try:
-                usia = int(input("Usia: "))
-            except ValueError:
-                print("Usia harus berupa angka.")
-                continue
+            while True:
+                try:
+                    usia = int(input("Usia: "))
+
+                    if usia <= 0:
+                        print("Usia harus lebih dari 0.")
+                        continue
+
+                    break
+
+                except ValueError:
+                    print("Usia harus berupa angka.")
 
             keluhan = sistem.pilih_keluhan()
 
             sistem.tambah_pasien(nama, usia, keluhan)
-            print("pasien")
+            input("\nTekan Enter untuk kembali...")
 
         elif pilihan == "2":
             sistem.lihat_antrean()
+            input("\nTekan Enter untuk kembali...")
 
         elif pilihan == "3":
             sistem.layani_pasien()
+            input("\nTekan Enter untuk kembali...")
 
         elif pilihan == "4":
             try:
@@ -47,9 +53,12 @@ def main():
                 sistem.cari_pasien(id)
             except ValueError:
                 print("ID harus berupa angka.")
+                continue
+            input("\nTekan Enter untuk kembali...")
 
         elif pilihan == "5":
             sistem.hitung_pasien()
+            input("\nTekan Enter untuk kembali...")
 
         elif pilihan == "6":
             print("Terima kasih telah menggunakan sistem ini.")
@@ -57,6 +66,7 @@ def main():
 
         else:
             print("Pilihan menu tidak valid.")
+            input("\nTekan Enter untuk kembali...")
 
 
 if __name__ == "__main__":
--- module.py
+++ module.py
@@ -1,4 +1,15 @@
 from datetime import datetime
+import os
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def header(judul):
+    lebar = 90
+
+    print("\n" + "=" * lebar)
+    print(judul.center(lebar))
+    print("=" * lebar)
 
 class Pasien:
     def __init__(self, id, nama, usia, keluhan):
@@ -9,7 +20,14 @@ class Pasien:
         self.waktu = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
     def __str__(self):
-        return f"ID: {self.id:03d} | Nama: {self.nama} | Usia: {self.usia} | Keluhan: {self.keluhan} | Waktu: {self.waktu}"
+        return f"ID: {self.id} Nama: {self.nama} Usia : {self.usia} Keluhan: {self.keluhan} Waktu: {self.waktu}"
+    
+    def tampilkan_detail(self):
+        print(f"ID      : {self.id}")
+        print(f"Nama    : {self.nama}")
+        print(f"Usia    : {self.usia}")
+        print(f"Keluhan : {self.keluhan}")
+        print(f"Waktu   : {self.waktu}")
 
 
 class SistemAntreanGigi:
@@ -27,14 +45,12 @@ class SistemAntreanGigi:
         self.id_berikutnya = 1
 
     def pilih_keluhan(self):
-        print("\n" + "=" * 40)
-        print(" PILIH KELUHAN PASIEN ")
-        print("=" * 40)
+        header("PILIH KELUHAN PASIEN")
 
         for i, keluhan in enumerate(self.KELUHAN, start=1):
             print(f"{i}. {keluhan}")
 
-        print("=" * 40)
+        print("=" * 90)
 
         while True:
             try:
@@ -80,17 +96,22 @@ class SistemAntreanGigi:
 
         pasien = self.antrean.pop(0)
 
-        print("\n===== SEDANG DILAYANI =====")
-        print(pasien)
+        header("PASIEN DILAYANI")
+        pasien.tampilkan_detail()
+        print("=" * 90)
 
     def cari_pasien(self, id):
         for pasien in self.antrean:
             if pasien.id == id:
-                print("\n===== PASIEN DITEMUKAN =====")
-                print(pasien)
+                header("PASIEN DITEMUKAN")
+
+                pasien.tampilkan_detail()
+                print("=" * 90)
                 return
 
         print("Pasien tidak ditemukan.")
 
     def hitung_pasien(self):
-        print(f"\nTotal pasien dalam antrean: {len(self.antrean)}")
+        print("\n" + "=" * 90)
+        print(f"Total pasien dalam antrean: {len(self.antrean)}")
+        print("=" * 90)
